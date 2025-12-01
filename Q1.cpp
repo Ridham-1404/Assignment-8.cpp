3f3d18fcef4c7329c77c41d2defaec010e224650
Ridham-1404 <rridham_be24@thapar.edu>
2025-12-01 17:41:21 +0530

    Create Q1.cpp

---
+++ Q1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+void inorder(node*root){
+    if(root==NULL)
+    return;
+    inorder(root->left);
+    cout<<root->data<<"  ";
+    inorder(root->right);
+}
+void preorder(node *root){
+    if (root == NULL)return;
+    cout<<root->data<<"  ";
+    preorder(root->left);
+    preorder(root->right);
+
+}
+void postorder(node *root){
+    if (root == NULL)return;
+    postorder(root->left);
+     cout<<root->data<<"  ";
+    postorder(root->right);
+
+}
